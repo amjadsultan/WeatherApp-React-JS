d921514a38686971df1d011fce2c301735f8be15
Amjad Sultan <122593928+amjadsultan@users.noreply.github.com>
2023-06-22 10:31:07 +0500

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "50a5848b2720667f4e64770a5c6dd1ab",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
